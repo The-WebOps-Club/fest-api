bddc4276ed79d5f32014b5e05f7370b033869d15
Shahidh <muhammedshahid.k@gmail.com>
2014-03-12 12:35:16 +0530

    Added secret key file, removed settings from git

--- configs/global_settings.py
+++ configs/global_settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hz3@sbz0q+wb&irbyn0h)cu9+9t7ofh@1tn3s!^)xia8_u$2+4'
+# SECRET_KEY = '' Moved to local_settings
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- configs/settings.py
+++
@@ -1,2 +0,0 @@
-import local_settings
-from local_settings import *
