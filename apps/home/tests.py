42fc5c6c3c965b99553912088914c07c086df4c8
AbdealiJK <abdealikothari@gmail.com>
2014-03-17 14:20:08 +0530

    Added py_filter in misc, but still doxygen doesnt work well

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -10,7 +10,43 @@ from django.test import TestCase
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
+         """
+        Does nothing more than demonstrate syntax.
+    
+        This is an example of how a Pythonic human-readable docstring can
+        get parsed by doxypypy and marked up with Doxygen commands as a
+        regular input filter to Doxygen.
+    
+        Args:
+            arg1:   A positional argument.
+            arg2:   Another positional argument.
+    
+        Kwargs:
+            kwarg:  A keyword argument.
+    
+        Returns:
+            A string holding the result.
+    
+        Raises:
+            ZeroDivisionError, AssertionError, & ValueError.
+    
+        Examples:
+            >>> myfunction(2, 3)
+            '5 - 0, whatever.'
+            >>> myfunction(5, 0, 'oops.')
+            Traceback (most recent call last):
+                ...
+            ZeroDivisionError: integer division or modulo by zero
+            >>> myfunction(4, 1, 'got it.')
+            '5 - 4, got it.'
+            >>> myfunction(23.5, 23, 'oh well.')
+            Traceback (most recent call last):
+                ...
+            AssertionError
+            >>> myfunction(5, 50, 'too big.')
+            Traceback (most recent call last):
+                ...
+            ValueError
         """
         self.assertEqual(1 + 1, 2)
+    
---
+++ misc/py_filter.py
@@ -0,0 +1,2 @@
+#!/bin/bash
+python -m doxy.py -a -c $1
