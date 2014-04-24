463aa36d4f9d759c28f740fbf2123d2853e41b1d
AbdealiJK <abdealikothari@gmail.com>
2014-04-24 08:24:55 +0530

    Loads of cleaning up. Frontend made cleaner

--- files/static/js/jquery.scripts.js
+++ files/static/js/jquery.scripts.js
@@ -1,21 +1,4 @@
 (function ($, undefined) {
-	$.fn.getCursorPosition = function() {
-		var el = $(this).get(0)
-		var pos = 0
-		if ( 'selectionStart' in el ) {
-			pos = el.selectionStart;
-		} else if ( "selection" in document ) {
-			el.focus()
-			var Sel = document.selection.createRange();
-			var SelLength = document.selection.createRange().text.length
-			Sel.moveStart("character", -el.value.length)
-			pos = Sel.text.length - SelLength;
-		} else {
-			console.log("Ouch : Cannot find selection !")
-		}
-		return pos
-	}            
-
 	$.fn.submit_form = function(v) {
 		el = $(this).get(0); $el = $(this)
 		if ( ! ( el.tagName == "INPUT" || el.tagName == "TEXTAREA" || 
@@ -30,16 +13,6 @@
 		}
 		console.log(my_form)
         my_form.submit()
-	}
-
-	$.fn.add_to_textarea = function(v) {
-		el = $(this).get(0); $el = $(this); 
-    	var cur_val = $el.val(), 
-    		cur_pos = $el.getCursorPosition(),
-    		append_str = v;
-    	$el.val(cur_val.substr(0, cur_pos) + append_str + cur_val.substr(cur_pos))
-     	$el.focus()
-     	el.selectionStart = el.selectionEnd = cur_pos + append_str.length
 	}
 })(jQuery)
 
