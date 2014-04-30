96b8c75e2f7df4c2c7588a555257aa435c1bed10
Phoenix25 <saipraveenb25@gmail.com>
2014-04-30 14:04:18 +0530

    modified local_storage.js

--- files/static/js/local_storage.js
+++ files/static/js/local_storage.js
@@ -4,14 +4,20 @@ function get_fbid_or_null( id ){
 	if(!localStorage.fbids)
 		return null;
 
-	fbids = JSON.parse(localStorage.fbids)
+	fbids = JSON.parse(localStorage.fbids);
 	if( fbids[id] != undefined )
-		return fbids[id]
+		return fbids[id];
 	else
 		return null;
 }
+
 function push_fbid( id, fbid ){
 	if( !localStorage.fbids )
-		localStorage.fbids = {}
-	localStorage.fbids[id] = fbid;
+		storage = {}
+	else
+		storage = JSON.stringify(localStorage.fbids);
+
+	storage[id] = fbid;
+
+	localStorage.fbids = JSON.parse(storage);
 }
