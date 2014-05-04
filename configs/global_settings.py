c5790a7dc95f18a490a38325a1510eae701c7441
Shahidh <muhammedshahid.k@gmail.com>
2014-05-04 12:32:17 +0530

    Commented Haystack for now, need to migrate for subjects

--- configs/global_settings.py
+++ configs/global_settings.py
@@ -473,9 +473,9 @@ if os.path.exists(GOOGLE_API_CREDENTIALS_FILE_PATH):
 
 # ----------------------------------------------------
 # Solr-Haystach search settings
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
-        'URL': 'http://127.0.0.1:8983/solr'
-    },
-}
+# HAYSTACK_CONNECTIONS = {
+#     'default': {
+#         'ENGINE': 'haystack.backends.solr_backend.SolrEngine',
+#         'URL': 'http://127.0.0.1:8983/solr'
+#     },
+# }
