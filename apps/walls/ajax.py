e2d45574e04cf31c107f171e473a99950e130893
Phoenix25 <saipraveenb25@gmail.com>
2014-05-10 15:21:40 +0530

    forgot to add a few files.. and fixed misaka.

--- apps/walls/ajax.py
+++ apps/walls/ajax.py
@@ -305,7 +305,7 @@ def create_comment(request, post_id, csrf_data, comment):
 
     comment_text, notification_list = parse_atwho(comment_text)
 
-    rendered_comment = comment_text#Template('{%load markdown_tags%}{{comment_text|markdown}}').render(Context({'comment_text':comment_text}))
+    rendered_comment = Template('{%load markdown_tags%}{{comment_text|markdown}}').render(RequestContext(request,{'comment_text':comment_text}))
 
     new_comment = Comment.objects.create(description=rendered_comment, by=request.user)
     post.comments.add(new_comment)
---
+++ misc/templatetags/unread_by_wall.py
@@ -0,0 +1,14 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.http.request import QueryDict
+#from BeautifulSoup import BeautifulSoup, NavigableString
+register = template.Library()
+
+@register.filter(name="unread_by_wall")
+def unread_by_wall(value,wid):
+	count = 0
+	for i in value.notifications.unread():
+		if (i.target.wall.id == wid):
+			count+=1
+
+	return count
