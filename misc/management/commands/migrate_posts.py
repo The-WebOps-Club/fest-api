3e53911ce2b251711e0e07bae7a80cb15e048b86
Phoenix25 <saipraveenb25@gmail.com>
2014-05-11 10:56:33 +0530

    fixed management command migrate_posts

--- misc/management/commands/migrate_posts.py
+++ misc/management/commands/migrate_posts.py
@@ -1,7 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError, NoArgsCommand
 from django.contrib.auth.models import User
 from apps.users.models import Dept, Subdept, ERPProfile
+from apps.walls.models import Comment, Post
 from django.conf import settings
+from django.template import Template,Context
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 import datetime
@@ -12,10 +14,29 @@ class Command(BaseCommand):
     """
     help = 'Automatically parses existing markdown based HTML'
 
-    def handle(self, pk_end,**options):
-        for c in Comment.objects.all():
-            if c.pk < pk_end:
-                rendered_comment = Template('{%load markdown_tags%}{%autoescape off%}{{comment_text|markdown}}{%endautoescape%}').render(RequestContext({'comment_text':c.description}))
-                c.description = rendered_comment
+    def handle(self, edit_all = 'false', debug = 'true',comment_end=-1, post_end=-1,**options):
+        for c in Comment.objects.all() :
+            if edit_all or ( comment_end != -1 and c.pk < comment_end) :
+                try:
+                    rendered_comment = Template('{%load markdown_tags%}{%autoescape off%}{{comment_text|markdown}}{%endautoescape%}').render(Context({'comment_text':c.description}))
+                    c.description = rendered_comment
+                    if debug:
+                        print "Parsed: "+format(c.pk)
+                except Exception as e:
+                    if debug:
+                        print(e.message)
+                    
+
+                c.save();
+
+        for c in Post.objects.all() :
+            if edit_all or ( post_end != -1 and c.pk < pk_end) :
+                try:
+                    rendered_comment = Template('{%load markdown_tags%}{%autoescape off%}{{post_text|markdown}}{%endautoescape%}').render(Context({'post_text':c.description}))
+                    c.description = rendered_comment
+                except Exception as e:
+                    if debug:
+                        print(e.message);
+
                 c.save();
 
