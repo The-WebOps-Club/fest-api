2c2d0a909af3c9ab66ea095c9f0c45195095ea23
Shahidh <muhammedshahid.k@gmail.com>
2014-05-31 19:47:22 +0530

    Added search funtionality for user, dept, subdept and posts

--- apps/search/ajax.py
+++ apps/search/ajax.py
@@ -2,6 +2,7 @@
 from dajaxice.decorators import dajaxice_register
 # From Django
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core import serializers
 
 # For rendering templates
 from django.template import RequestContext
@@ -11,6 +12,8 @@ from django.template.loader import render_to_string
 from django.contrib.auth.decorators import login_required, user_passes_test
 import json
 
+from haystack.query import SearchQuerySet
+
 @dajaxice_register
 def hello(request, query):
     """
@@ -19,3 +22,10 @@ def hello(request, query):
     #html_content = render_to_string("dash/task_tables/coord_you.html", query_dictionary, RequestContext(request))
     return json.dumps({'query': query})
 
+
+
+@dajaxice_register
+def query(request, query):
+	results = SearchQuerySet().filter(content=query)
+	results_list = [q.get_stored_fields() for q in results]
+	return json.dumps(results_list)
--- apps/walls/search_indexes.py
+++ apps/walls/search_indexes.py
@@ -1,11 +1,15 @@
 from haystack import indexes
-from models import Post, Comment, Wall
+from models import Post
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 class PostIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
-    # post_subject = indexes.CharField(model_attr='subject')
-    # post_description = indexes.CharField(model_attr='description')
-    # pub_date = indexes.DateTimeField(model_attr='pub_date')
+    subject = indexes.CharField(model_attr='subject')
+    description = indexes.CharField(model_attr='description')
+    created = indexes.CharField(model_attr='time_created')
+    author = indexes.CharField(model_attr='by')
+    wall = indexes.CharField(model_attr='wall')
+    url = indexes.CharField()
 
     def get_model(self):
         return Post
@@ -14,22 +18,14 @@ class PostIndex(indexes.SearchIndex, indexes.Indexable):
         """Used when the entire index for model is updated."""
         return self.get_model().objects.all()
 
-class CommentIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
+    def prepare_author(self, obj):
+    	return obj.by.get_full_name()
+    	
+    def prepare_wall(self, obj):
+    	return obj.wall.name
 
-    def get_model(self):
-        return Comment
+    def prepare_created(self, obj):
+    	return naturaltime(obj.time_created)
 
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
-
-class WallIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True)
-
-    def get_model(self):
-        return Wall
-
-    def index_queryset(self, using=None):
-        """Used when the entire index for model is updated."""
-        return self.get_model().objects.all()
+    def prepare_url(self, obj):
+    	return obj.get_absolute_url()
