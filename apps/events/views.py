4516a866d957264b66fa271c18c5a696f8be6c4b
Akshay <akshay@akshay-Lenovo-IdeaPad-Z500.(none)>
2014-07-05 14:35:26 +0530

    Edit Event Tab button added at apps/events/ . Accessible at localhost:8000/events/add_tabs.

--- apps/events/views.py
+++ apps/events/views.py
@@ -22,13 +22,14 @@ from misc.utils import global_context
 
 
 from apps.events.models import Event, EventTab
-from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-@login_required
+
 def portal_main2( request ):
 	message=""
+#If add tab button was pressed
 	if request.method == 'POST' and "addNewTab" in request.POST:
-		if request.POST['tab_name']!='' :
+		if request.POST['tab_name']!='' and  request.POST['tab_name'][0]!=' ':
 			print "yuu"
 	    		event_tab=EventTab()
 			event_tab.name=request.POST['tab_name']
@@ -36,13 +37,30 @@ def portal_main2( request ):
 			event_tab.event=Event.objects.get(name=request.POST['event_name'])
 			event_tab.save()
 			message="The " + request.POST['tab_name'] + " tab has been successfully added to the " + request.POST['event_name'] + " event"
-	    
+#If delete tab button was pressed	    
 	if request.method == 'POST' and "delete_tab" in request.POST:
 		print "dd"
 		event_object=Event.objects.get(name=request.POST['eventName'])
 		event_tab=EventTab.objects.get(name=request.POST['event_tab_name'],event=event_object)
 		event_tab.delete()
     		message="The "+ request.POST['event_tab_name']+" tab in the event " + request.POST['eventName']+ " has been successfully deleted"
+#If edit tab button was pressed
+	if request.method == 'POST' and "EditTab" in request.POST:
+		if request.POST['tab_Name']!='' and  request.POST['tab_Name'][0]!=' ':
+			event_object=Event.objects.get(name=request.POST['event_Name_edit_form'])
+			event_Tab=EventTab.objects.get(name=request.POST['event_tab_Name_edit_form'],event=event_object)
+			event_Tab.delete()
+
+			event_tab=EventTab()
+			event_tab.name=request.POST['tab_Name']
+			event_tab.content=request.POST['tab_Description']
+			event_tab.event=Event.objects.get(name=request.POST['event_Name_edit_form'])
+			event_tab.save()
+
+			print "qqqqqqqqu"
+			message="The " + request.POST['tab_Name'] + " tab from the event " + request.POST['event_Name_edit_form'] + "  has been successfully Edited."
+
+
 	events=Event.objects.all()
 	event_list=[]
 	for i in events:
