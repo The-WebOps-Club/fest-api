fe74b92bc316a93330abd715cf24a4882886bfb8
Akshay Utture <akshay.utture@gmail.com>
2014-07-09 01:41:08 +0530

    Permissions added (user has to be a events/qms core to access the edit/delete/add event-tab button

--- apps/events/ajax.py
+++ apps/events/ajax.py
@@ -20,27 +20,43 @@ def hello(request):
 
 
 
-from apps.events.models import EventTab, Event
+#dajaxice stuff
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+#models
+from django.contrib.auth.models import User
+from apps.users.models import ERPProfile, UserProfile, Dept, Subdept
+from apps.events.models import EventTab, Event
+
+
+
 @dajaxice_register
-def show_tabs(request,event_name):
+def show_tabs(request,event_name,username):
     event_object=Event.objects.get(name=event_name)
+    user_object=User.objects.get(username=username)
+    has_perm = permission(event_object,user_object)
     tabs_object_list=event_object.eventtab_set.all()
 #tabs_names_list is a string with a set of event-tab-names -separated by commas
     tabs_names_list=''
     for i in tabs_object_list:
 	tabs_names_list=tabs_names_list+i.name+','
-    return simplejson.dumps({'tabs_names_list':'%s' % tabs_names_list,'event_name':event_name})
+    return simplejson.dumps({'tabs_names_list':'%s' % tabs_names_list,'event_name':event_name,'has_perm':has_perm})
 
 
 @dajaxice_register
-def show_tabs_description(request,event_name,event_tab):
+def show_tabs_description(request,event_name,event_tab,has_perm):
     event_object=Event.objects.get(name=event_name)
     event_tab=EventTab.objects.get(name=event_tab,event=event_object)
     description=event_tab.content
-    return simplejson.dumps({'description': description,'event_name':event_name,'event_tab_name': event_tab.name})
+    return simplejson.dumps({'description': description,'event_name':event_name,'event_tab_name': event_tab.name,'has_perm':has_perm})
 
 
-
+#Function for setting permissions to edit Event Tabs 
+def permission(event_object,user_object):
+	events_dept=Dept.objects.get(name='events')
+	qms_dept=Dept.objects.get(name='qms')
+	if events_dept in user_object.erp_profile.core_relations.all() or qms_dept in user_object.erp_profile.core_relations.all():
+		return "yes"
+	else:
+		return "no"
 
