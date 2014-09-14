c1968f5eb27193fc4dc52ce1b624259bfd2f0c81
Mohit Kumar <mhtkmr54@gmail.com>
2014-09-14 15:01:03 +0530

    called function to populate in ajax.py,and removed merge conflicts

---
+++ files/static-root/emails/configs/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for fest_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+
+
+# Set the DJANGO_SETTINGS_MODULE environment variables
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings")
+
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
