a3b9771a922631adc0adbf52086c61e2f896da7e
Shahidh <muhammedshahid.k@gmail.com>
2014-10-15 23:11:41 +0530

    Made visible available in ERP

--- apps/portals/events/forms.py
+++ apps/portals/events/forms.py
@@ -5,4 +5,4 @@ from apps.events.models import Event
 class AddEventForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields=('name','short_description','event_type','category','has_tdp','team_size_min','team_size_max','registration_starts','registration_ends','google_group','email','coords',)
+        fields=('name','is_visible','short_description','event_type','category','has_tdp','team_size_min','team_size_max','registration_starts','registration_ends','google_group','email','coords',)
