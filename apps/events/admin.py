f7a68bb3b9c7765b03e436fb3ad8aa7a70821216
shahidhk <muhammedshahid.k@gmail.com>
2014-11-09 20:32:08 +0530

    Lots of modifications for events

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import EventTab, Event
+from models import EventTab, Event, EventRegistration
 
 class EventTabAdmin(admin.ModelAdmin):
     list_display=('pk','event','name' )
@@ -7,5 +7,9 @@ admin.site.register(EventTab,EventTabAdmin)
 
 class EventAdmin(admin.ModelAdmin):
     list_display=('pk','name','event_type', "category" )
-
 admin.site.register(Event,EventAdmin)
+
+class EventRegistrationAdmin(admin.ModelAdmin):
+    list_display=('pk','event', 'users_registered', 'info', 'teams_registered')
+    search_fields=['users_registered']
+admin.site.register(EventRegistration, EventRegistrationAdmin)
--- apps/portals/events/admin.py
+++ apps/portals/events/admin.py
@@ -1,13 +1,3 @@
 
-from django.contrib import admin
 
-from apps.events.models import Tab
 
-from apps.events.models import EventRegistration
-
-#admin.site.register(Event)
-admin.site.register(Tab)
-
-#new
-#admin.site.register(EventTab)
-admin.site.register(EventRegistration)
