7129e424b39eecd5e1f76a8d53e20bf2795ba1bb
Abdeali JK <abdealikothari@gmail.com>
2014-11-26 09:48:58 +0530

    Fixed space/tabs issue

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -8,46 +8,47 @@ from apps.walls.models import Wall, Post, Comment
 from apps.blog.models import Category
 
 class UserSerializer(serializers.ModelSerializer):
-    want_accomodation = serializers.BooleanField(source='profile.want_accomodation')
+    want_accomodation = serializers.BooleanField(source='profile.want_accomodation', required=False)
     mobile_number = serializers.CharField(source='profile.mobile_number', required=False)
     college_text = serializers.CharField(source='profile.college_text', required=False)
     class Meta:
-		model = User
-		fields = ('id', 'first_name', 'last_name', 'email', 'password')
+        model = User
+        # fields = ('id', 'first_name', 'last_name', 'email', 'password')
+        exclude = ("password", "user_permissions", "username", "last_login", "is_staff", "is_superuser", "is_active", "groups", "date_joined")
 
 class UserProfileSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = ERPProfile
+    class Meta:
+        model = ERPProfile
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
-		model = Event
+        model = Event
 
 class ParticipantProfileSerializer(serializers.ModelSerializer):
     class Meta:
-		model = UserProfile
+        model = UserProfile
 
 class TeamSerializer(serializers.ModelSerializer):
-	members = UserSerializer(source='members', many=True)
-	class Meta:
-		model = Team
+    members = UserSerializer(source='members', many=True)
+    class Meta:
+        model = Team
 
 class WallSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Wall
-		fields=('id','name','is_public','time_updated','cache_updated','person')
-		depth=2
+    class Meta:
+        model = Wall
+        fields=('id','name','is_public','time_updated','cache_updated','person')
+        depth=2
 class PostSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Post
-		fields = ('id','is_active','subject','by','description','time_created','time_updated','comments')
-		depth = 2
+    class Meta:
+        model = Post
+        fields = ('id','is_active','subject','by','description','time_created','time_updated','comments')
+        depth = 2
 
 class CommentSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Comment
-		fields=('id','is_active','access_specifier','description','by','time_created','time_updated','liked_users')
-		depth = 1
+    class Meta:
+        model = Comment
+        fields=('id','is_active','access_specifier','description','by','time_created','time_updated','liked_users')
+        depth = 1
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
